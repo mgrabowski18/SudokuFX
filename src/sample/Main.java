332cab89fe18a8a809ec93f06cf8740579a2684b
Marcin Grabowski <mgrabowski18@gmail.com>
2019-03-05 15:26:12 +0100

    Implemented Sudoku Board.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,8 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Sudoku");
-        primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
+        primaryStage.setScene(new Scene(root, 600, 600));
+
     }
 
 
